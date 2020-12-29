63c64d9b42ac527cf3860f8c611f7a965343b6a6
William Luke <william@ordino.ai>
2020-12-29 11:55:16 +0300

    log env

--- scripts/log.js
+++ scripts/log.js
@@ -8,5 +8,6 @@ async function main(){
   console.log({".prisma/client":data.stdout});
   const indexJS = await execa('cat', ['./index.js'], {cwd: path.join(__dirname, '..', 'node_modules', '.prisma', 'client')});
   console.log({"index.js":indexJS.stdout});
+  console.log({env: process.env});
 }
 main()
